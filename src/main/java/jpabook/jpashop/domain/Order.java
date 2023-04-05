a58c7efad8e5cb26d8384a6fa35cae99e89dd9f9
minu <made_power@naver.com>
2023-04-06 03:56:02 +0900

    JPA에서 DTO 직접 조회
    JPA에서 직접 DTO를 조회하기위해 jpql를 추가해서 최적화하였다. 하지만 N+1 문제가 발생했다

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -26,7 +26,7 @@ public class Order {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name ="member_id")
     private  Member member;
-    
+
     @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
     @OneToOne(fetch = FetchType.LAZY, cascade = ALL)
