18588828b0215aca6362eb8d4b4805d27a5685ce
minu <made_power@naver.com>
2023-04-06 03:10:14 +0900

    API 최적화
    v3.0은 쿼리를 한번에 불러오는 장점이 있었지만 데이터가 많아지는 단점이 존재하여 배치 사이즈를 늘려줌으로써 원하는 데이터를 가져오도록 하였다.

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -25,6 +26,7 @@ public class Order {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name ="member_id")
     private  Member member;
+    
     @OneToMany(mappedBy = "order", cascade = ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
     @OneToOne(fetch = FetchType.LAZY, cascade = ALL)
